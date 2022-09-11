697c0cd045e4f1f88e51be15a59303f27f0becc6
Elias Farhan <elias.farhan@gmail.com>
2022-09-11 09:48:18 +0200

    adding constexpr color

---
+++ core/include/graphics/color.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <SFML/Graphics/Color.hpp>
+#include <cstdint>
+
+namespace core
+{
+    
+struct Color
+{
+    std::uint8_t r = 0u;
+    std::uint8_t g = 0u;
+    std::uint8_t b = 0u;
+    std::uint8_t a = 0u;
+    constexpr Color() = default;
+    constexpr Color(std::uint8_t r, std::uint8_t g, std::uint8_t b, std::uint8_t a = 255u): r(r), g(g), b(b), a(a){}
+
+    operator sf::Color() const { return {r, g, b, a}; }
+
+    static constexpr Color red() { return { 255u,0u,0u,255u }; }
+    static constexpr Color green() { return { 0u,255u,0u,255u }; }
+    static constexpr Color blue() { return { 0u,0u,255u,255u }; }
+    static constexpr Color yellow() { return { 255u,255u,0u,255u }; }
+    static constexpr Color black() { return { 0u,0u,0u,255u }; }
+    static constexpr Color white() { return { 255u,255u,255u,255u }; }
+    static constexpr Color magenta() { return { 255u,0u,255u,255u }; }
+    static constexpr Color cyan() { return { 0u,255u,255u,255u }; }
+    static constexpr Color transparent() { return { 0u,0u,0u,0u }; }
+};
+
+} // namespace core
