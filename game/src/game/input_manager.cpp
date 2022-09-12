ea99a5fc6e93f9699b4ea39c468775a80d273fd4
Elias Farhan <elias.farhan@gmail.com>
2022-09-12 09:44:06 +0200

    Correct some warnings on linux

--- game/src/game/input_manager.cpp
+++ game/src/game/input_manager.cpp
@@ -11,22 +11,32 @@ PlayerInput GetPlayerInput(int index)
     case 0:
     {
         PlayerInput clientInput1 = 0;
-        clientInput1 = clientInput1 | (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) ? PlayerInputEnum::PlayerInput::LEFT : 0u);
-        clientInput1 = clientInput1 | (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) ? PlayerInputEnum::PlayerInput::RIGHT : 0u);
-        clientInput1 = clientInput1 | (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) ? PlayerInputEnum::PlayerInput::UP : 0u);
-        clientInput1 = clientInput1 | (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) ? PlayerInputEnum::PlayerInput::DOWN : 0u);
-        clientInput1 = clientInput1 | (sf::Keyboard::isKeyPressed(sf::Keyboard::RControl) ? PlayerInputEnum::PlayerInput::SHOOT : 0u);
+        clientInput1 = clientInput1 | (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) ?
+                PlayerInputEnum::PlayerInput::LEFT : PlayerInputEnum::PlayerInput::NONE);
+        clientInput1 = clientInput1 | (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) ?
+                PlayerInputEnum::PlayerInput::RIGHT : PlayerInputEnum::PlayerInput::NONE);
+        clientInput1 = clientInput1 | (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) ?
+                PlayerInputEnum::PlayerInput::UP : PlayerInputEnum::PlayerInput::NONE);
+        clientInput1 = clientInput1 | (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) ?
+                PlayerInputEnum::PlayerInput::DOWN : PlayerInputEnum::PlayerInput::NONE);
+        clientInput1 = clientInput1 | (sf::Keyboard::isKeyPressed(sf::Keyboard::RControl) ?
+                PlayerInputEnum::PlayerInput::SHOOT : PlayerInputEnum::PlayerInput::NONE);
 
         return clientInput1;
     }
     case 1:
     {
         PlayerInput clientInput2 = 0;
-        clientInput2 = clientInput2 | (sf::Keyboard::isKeyPressed(sf::Keyboard::A) ? PlayerInputEnum::PlayerInput::LEFT : 0u);
-        clientInput2 = clientInput2 | (sf::Keyboard::isKeyPressed(sf::Keyboard::D) ? PlayerInputEnum::PlayerInput::RIGHT : 0u);
-        clientInput2 = clientInput2 | (sf::Keyboard::isKeyPressed(sf::Keyboard::W) ? PlayerInputEnum::PlayerInput::UP : 0u);
-        clientInput2 = clientInput2 | (sf::Keyboard::isKeyPressed(sf::Keyboard::S) ? PlayerInputEnum::PlayerInput::DOWN : 0u);
-        clientInput2 = clientInput2 | (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) ? PlayerInputEnum::PlayerInput::SHOOT : 0u);
+        clientInput2 = clientInput2 | (sf::Keyboard::isKeyPressed(sf::Keyboard::A) ?
+                PlayerInputEnum::PlayerInput::LEFT : PlayerInputEnum::PlayerInput::NONE);
+        clientInput2 = clientInput2 | (sf::Keyboard::isKeyPressed(sf::Keyboard::D) ?
+                PlayerInputEnum::PlayerInput::RIGHT : PlayerInputEnum::PlayerInput::NONE);
+        clientInput2 = clientInput2 | (sf::Keyboard::isKeyPressed(sf::Keyboard::W) ?
+                PlayerInputEnum::PlayerInput::UP : PlayerInputEnum::PlayerInput::NONE);
+        clientInput2 = clientInput2 | (sf::Keyboard::isKeyPressed(sf::Keyboard::S) ?
+                PlayerInputEnum::PlayerInput::DOWN : PlayerInputEnum::PlayerInput::NONE);
+        clientInput2 = clientInput2 | (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) ?
+                PlayerInputEnum::PlayerInput::SHOOT : PlayerInputEnum::PlayerInput::NONE);
         return clientInput2;
     }
     default:
